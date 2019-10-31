6d1a286d5a0c5403e4853252130a71a821731527
WuSalmon <salmon918@gmail.com>
2019-11-01 02:18:50 +0800

    feat: 完成PC版

--- dist/assets/cms/js/main.js
+++ dist/assets/cms/js/main.js
@@ -1,7 +1,15 @@
 $(function () {
+    $(window).on('scroll', function(){
+        if ($(window).scrollTop() >= $('#header').outerHeight() - 400) {
+            $('.go-top').addClass('show');
+        } else {
+            $('.go-top').removeClass('show');
+        }
+    });
+
     $('.anchor').on('click', function () {
         $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top - 200
+            scrollTop: $($.attr(this, 'data-href')).offset().top - 100
         }, 800);
     })
 });
