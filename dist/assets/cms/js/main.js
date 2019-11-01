cce5bae212b1785492c7cb64770ccfe814768271
Salmon918 <salmon918@gmail.com>
2019-11-01 11:48:26 +0800

    fix: 修改breakpoint

--- dist/assets/cms/js/main.js
+++ dist/assets/cms/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    $(window).on('scroll', function(){
+    $(window).on('scroll', function () {
         if ($(window).scrollTop() >= $('#header').outerHeight() - 400) {
             $('.go-top').addClass('show');
         } else {
@@ -14,36 +14,36 @@ $(function () {
     })
 
     // nav 按鈕
-    $('.btn-menu').on('click',function () {
-        if(!$(this).hasClass('opened')){
-            $('.menu-list').css('display','block');
+    $('.btn-menu').on('click', function () {
+        if (!$(this).hasClass('opened')) {
+            $('.menu-list').css('display', 'block');
             $(this).addClass('opened');
-        }else {
-            $('.menu-list').css('display','none');
+        } else {
+            $('.menu-list').css('display', 'none');
             $(this).removeClass('opened');
         }
     });
 
-    $('.menu-list').on('click',function () {
-        $('.menu-list').css('display','none');
+    $('.menu-list').on('click', function () {
+        $('.menu-list').css('display', 'none');
         $('.btn-menu').removeClass('opened');
     });
 
     // resize 設定計時器處理
     var resizeTimer = null;
-    $(window).on('resize', function() {
+    $(window).on('resize', function () {
         if (resizeTimer) {
             clearTimeout(resizeTimer)
         }
-        resizeTimer = setTimeout(function(){
+        resizeTimer = setTimeout(function () {
 
             //== resize function START====
             // console.log($(window).innerWidth());
-            if($(window).innerWidth() < 768){
-                $('.menu-list').css('display','none');
+            if ($(window).innerWidth() < 768) {
+                $('.menu-list').css('display', 'none');
                 $('.btn-menu').removeClass('opened');
-            }else{
-                $('.menu-list').css('display','block');
+            } else {
+                $('.menu-list').css('display', 'block');
             }
             //== resize function END====
 
