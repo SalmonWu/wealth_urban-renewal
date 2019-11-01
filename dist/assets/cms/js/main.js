8cbd56b292b973da776d163cee1fcef6af1a5832
Sinyi <buns3366@gmail.com>
2019-11-01 10:43:44 +0800

    feat: 完成mobile版 nav

--- dist/assets/cms/js/main.js
+++ dist/assets/cms/js/main.js
@@ -12,4 +12,43 @@ $(function () {
             scrollTop: $($.attr(this, 'data-href')).offset().top - 100
         }, 800);
     })
+
+    // nav 按鈕
+    $('.btn-menu').on('click',function () {
+        if(!$(this).hasClass('opened')){
+            $('.menu-list').css('display','block');
+            $(this).addClass('opened');
+        }else {
+            $('.menu-list').css('display','none');
+            $(this).removeClass('opened');
+        }
+    });
+
+    $('.menu-list').on('click',function () {
+        $('.menu-list').css('display','none');
+        $('.btn-menu').removeClass('opened');
+    });
+
+    // resize 設定計時器處理
+    var resizeTimer = null;
+    $(window).on('resize', function() {
+        if (resizeTimer) {
+            clearTimeout(resizeTimer)
+        }
+        resizeTimer = setTimeout(function(){
+
+            //== resize function START====
+            // console.log($(window).innerWidth());
+            if($(window).innerWidth() < 768){
+                $('.menu-list').css('display','none');
+                $('.btn-menu').removeClass('opened');
+            }else{
+                $('.menu-list').css('display','block');
+            }
+            //== resize function END====
+
+        }, 300);
+    });
+
+
 });
